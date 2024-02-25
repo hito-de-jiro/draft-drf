e214911a3e0ee0fb588ca15b1cbb89e5b8d73ccf
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-02-25 20:51:30 +0200

    chore: check how to work serializers in drf

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from api.views import WomanApiView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/womenlist/', WomanApiView.as_view(), name='womenlist'),
 ]
