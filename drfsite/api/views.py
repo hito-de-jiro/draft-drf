813b42cbe0ec8bd81a68ba6a47bea1c9f105001b
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-02-26 13:48:16 +0200

    chore: serializers.py use class ModelSerializer, views.py use class ListCreateAPIView, refactor urls

--- drfsite/api/views.py
+++ drfsite/api/views.py
@@ -7,48 +7,6 @@ from .models import Women
 from .serializers import WomenSerializer
 
 
-# class WomanApiView(generics.ListAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
-
-class WomanApiView(APIView):
-    def get(self, request):
-        women = Women.objects.all().values()
-        return Response({'post': WomenSerializer(women, many=True).data}, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            # post_new = Women.objects.create(
-            #     title=request.data['title'],
-            #     content=request.data['content'],
-            #     category_id=request.data['category_id'],
-            # )
-            serializer.save()
-
-            # return Response({'title': WomenSerializer(post_new).data}, status=status.HTTP_201_CREATED)
-            return Response({'title': serializer.data}, status=status.HTTP_201_CREATED)
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs['pk']
-        if not pk:
-            return Response({'error': 'Method PUT not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Object does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = WomenSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"post": serializer.data}, status=status.HTTP_201_CREATED)
-
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs['pk']
-        try:
-            instance = Women.objects.get(pk=pk)
-            instance.delete()
-            return Response({"post": "Object deleted"}, status=status.HTTP_204_NO_CONTENT)
-        except:
-            return Response({'error': 'Object does not exist'}, status=status.HTTP_404_NOT_FOUND)
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from api.views import WomanApiView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomanApiView.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomanApiView.as_view()),
+    path('api/', include('api.urls')),
 ]
