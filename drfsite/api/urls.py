b6338ff1a6452949a9615868eeb0ff8faf4df6ae
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-02-27 10:06:37 +0200

    feth: added viewsets

--- drfsite/api/urls.py
+++ drfsite/api/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from api.views import (
-    WomenAPIList,
-    WomenAPIUpdate,
-    WomenAPIDetailView,
+    # WomenAPIList,
+    # WomenAPIUpdate,
+    # WomenAPIDetailView,
+    WomenViewSet,
 )
 
+
 urlpatterns = [
-    path('v1/womenlist/', WomenAPIList.as_view()),
-    path('v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
-    path('v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
+    # path('v1/womenlist/', WomenAPIList.as_view()),
+    # path('v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
+    # path('v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
+    path('v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    path('v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
 ]
--- drfsite/api/views.py
+++ drfsite/api/views.py
@@ -1,25 +1,32 @@
-from rest_framework import generics
+from rest_framework import viewsets
 
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenAPIList(generics.ListCreateAPIView):
-    """Displays all elements and creates a new element,
-    methods GET and POST """
-    queryset = Women.objects.all()
+# class WomenAPIList(generics.ListCreateAPIView):
+#     """Displays all elements and creates a new element,
+#     methods GET and POST """
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     """Updates a single,
+#     methods PUT and PATCH"""
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     """Displays a single,
+#      methods GET, PUT, PATCH and DELETE"""
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+class WomenViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing instances.
+    """
     serializer_class = WomenSerializer
-
-
-class WomenAPIUpdate(generics.UpdateAPIView):
-    """Updates a single,
-    methods PUT and PATCH"""
     queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-
-
-class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """Displays a single,
-     methods GET, PUT, PATCH and DELETE"""
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
