9f2c77fbd336166f2649ba7e4194958d1bf90448
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-02-27 09:15:42 +0200

    chore: crud - drf views

--- drfsite/api/urls.py
+++ drfsite/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from api.views import WomenAPIList
+from api.views import (
+    WomenAPIList,
+    WomenAPIUpdate,
+    WomenAPIDetailView,
+)
 
 urlpatterns = [
     path('v1/womenlist/', WomenAPIList.as_view()),  # GET and POST requests
+    path('v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),  # PUT and PATCH requests
+    path('v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),  # GET and POST requests
 ]
