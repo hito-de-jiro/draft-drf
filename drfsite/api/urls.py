f45b14e93bd0dba9c5e402f75673ea9157e4f557
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-02-27 10:16:14 +0200

    fetch: added routers to urls.py

--- drfsite/api/urls.py
+++ drfsite/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from api.views import (
     # WomenAPIList,
@@ -7,11 +7,16 @@ from api.views import (
     WomenViewSet,
 )
 
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'women', WomenViewSet)
 
 urlpatterns = [
+    path('api/v1/', include(router.urls))
+    # path('v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    # path('v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
     # path('v1/womenlist/', WomenAPIList.as_view()),
     # path('v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
     # path('v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
-    path('v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
-    path('v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
 ]
