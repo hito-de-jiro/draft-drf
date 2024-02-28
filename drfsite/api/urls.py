48ddd4110b84eca74eefc8804a7a2a07e3d2cff1
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2024-02-28 12:53:08 +0200

    chore: authentication user

--- drfsite/api/urls.py
+++ drfsite/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include, re_path
 
 from api.views import (
     WomenAPIList,
@@ -7,7 +7,10 @@ from api.views import (
 )
 
 urlpatterns = [
+    path('api/v1/drf-auth/', include('rest_framework.urls')),  # http://127.0.0.1:8000/api/v1/drf-auth/login/
     path('api/v1/women/', WomenAPIList.as_view()),
     path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
+    path('api/v1/auth/', include('djoser.urls')),  # http://127.0.0.1:8000/api/v1/auth/
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
